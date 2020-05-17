1c85f12487dc374215bacfacff48a4ab20fb723d
lord_haffi <spammy1335@gmail.com>
2020-05-17 17:48:30 +0200

    django homepage setup

--- PulpScience/settings.py
+++ PulpScience/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'PulpScience.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'homepage/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = '/assets/'
+STATICFILES_DIRS = [
+    '/homepage/static',
+]
