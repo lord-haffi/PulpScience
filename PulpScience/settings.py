9c5e8d7d52671a7d072977e9c818ed5f10d29999
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-01-19 00:41:27 +0100

    Fix test setup

--- PulpScience/settings.py
+++ PulpScience/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "PulpScience.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(os.path.dirname(BASE_DIR), "PulpScience/db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
