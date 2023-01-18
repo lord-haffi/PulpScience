592541ff21598981ae1b10abcb4eaca7f8b1f4d1
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-01-19 00:46:39 +0100

    Fix type checker

--- homepage/models.py
+++ homepage/models.py
@@ -3,7 +3,7 @@ This module contains all database models for django.
 """
 from django.db import models  # type:ignore[import]
 from django.utils import timezone  # type:ignore[import]
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy  # type:ignore[import]
 
 
 class Categories(models.TextChoices):
