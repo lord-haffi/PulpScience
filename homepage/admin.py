4c9c17e31c8dcfefbceef67153c2d6309974dc7e
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-01-19 14:50:46 +0100

    Register models to admin page

--- homepage/admin.py
+++ homepage/admin.py
@@ -4,4 +4,12 @@ Register your models here.
 # pylint: disable=unused-import
 from django.contrib import admin
 
+from homepage import models
+
 # Register your models here.
+admin.site.register(models.Followable)
+admin.site.register(models.Commentable)
+admin.site.register(models.Versionable)
+admin.site.register(models.Comment)
+admin.site.register(models.User)
+admin.site.register(models.Project)
