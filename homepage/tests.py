d6a19358f9ae3f192cd7ab06f73c9ae3a2b78cb9
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-01-21 12:46:07 +0100

    Test categories on index page

--- homepage/tests.py
+++ homepage/tests.py
@@ -3,5 +3,14 @@ Includes all unit and integration tests for the homepage app.
 """
 # pylint: disable=unused-import
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class TestIndexPage(TestCase):
+    def test_categories(self):
+        response = self.client.get(reverse("homepage:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Physics")
+        self.assertIn("categories", response.context)
+        # self.assertQuerysetEqual(response.context['latest_question_list'], [])
