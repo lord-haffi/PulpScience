7f4f8920d604004bff69661791ff9cddd48298d5
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-01-25 14:09:39 +0100

    Add docstrings

--- homepage/tests.py
+++ homepage/tests.py
@@ -9,11 +9,21 @@ from django.utils.translation import gettext_lazy
 
 # Create your tests here.
 class TestIndexPage(TestCase):
-    def test_template_used(self):
+    """
+    Including all tests for the index page.
+    """
+
+    def test_template_used(self) -> None:
+        """
+        Test if the index view uses the correct template file
+        """
         response = self.client.get(reverse("homepage:index"))
         self.assertTemplateUsed(response, "homepage/index.html")
 
-    def test_categories(self):
+    def test_categories(self) -> None:
+        """
+        Test if the categories are present and ok in views context.
+        """
         response = self.client.get(reverse("homepage:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Physics")
