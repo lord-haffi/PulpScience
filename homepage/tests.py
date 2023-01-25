8b0e914c1d14aa9345c1b23a427d1b880310b089
Leon Haffmans <49658102+lord-haffi@users.noreply.github.com>
2023-01-25 18:08:36 +0100

    Make categories on index page clickable (#16)
    * Make categories clickable for routing
    
    * Add tests
    
    * Fix linter

--- homepage/tests.py
+++ homepage/tests.py
@@ -2,17 +2,19 @@
 Includes all unit and integration tests for the homepage app.
 """
 # pylint: disable=unused-import
-from django.test import TestCase
+from django.test import TestCase, tag
 from django.urls import reverse
 from django.utils.translation import gettext_lazy
 
 
 # Create your tests here.
+@tag("page-index")
 class TestIndexPage(TestCase):
     """
     Including all tests for the index page.
     """
 
+    @tag("template")
     def test_template_used(self) -> None:
         """
         Test if the index view uses the correct template file
@@ -20,6 +22,7 @@ class TestIndexPage(TestCase):
         response = self.client.get(reverse("homepage:index"))
         self.assertTemplateUsed(response, "homepage/index.html")
 
+    @tag("context")
     def test_categories(self) -> None:
         """
         Test if the categories are present and ok in views context.
@@ -28,4 +31,31 @@ class TestIndexPage(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Physics")
         self.assertIn("categories", response.context)
-        self.assertIn(("PHY", gettext_lazy("Physics")), response.context["categories"])
+        has_physics = False
+        for category in response.context["categories"]:
+            self.assertIn("label", category)
+            if category["label"] == gettext_lazy("Physics"):
+                has_physics = True
+                break
+        self.assertTrue(has_physics)
+
+    @tag("routing")
+    def test_category_subrouting(self) -> None:
+        """
+        Test if the category subrouting responses.
+        """
+        response = self.client.get(reverse("homepage:index", kwargs={"active_category": "physics"}))
+        self.assertEqual(response.status_code, 200)
+
+    @tag("html")
+    def test_category_subrouting_html(self) -> None:
+        """
+        Test if the category subrouting works properly aka returns proper HTML.
+        """
+        response = self.client.get(reverse("homepage:index", kwargs={"active_category": "physics"}))
+        self.assertEqual(response.status_code, 200)
+        find_physics = (
+            f'<a class="topic-link active" href="'
+            f'{reverse("homepage:index", kwargs={"active_category": "physics"})}">Physics</a>'
+        )
+        self.assertContains(response, find_physics, html=True)
--- homepage/urls.py
+++ homepage/urls.py
@@ -1,11 +1,16 @@
 """
 Include all routings of the homepage app
 """
-from django.urls import path
+from django.urls import path, register_converter
 
 from . import views
+from .converters import CategoryConverter
 
 APP_NAME = "homepage"
+
+register_converter(CategoryConverter, "category")
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("category/<category:active_category>", views.index, name="index"),
 ]
