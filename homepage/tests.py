72e8bbd4a5d8c2aed2377fbeb143a6aa11114671
Leon Haffmans <49658102+lord-haffi@users.noreply.github.com>
2023-05-10 19:01:12 +0200

    Add a basic login page and integrate it to the index page (#30)
    * Add login page
    
    * Add tests for basic login page
    
    * Fix tests
    
    * fix linter

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,6 +1,8 @@
 """
 Includes all unit and integration tests for the homepage app.
 """
+from django.contrib.auth.models import User
+
 # pylint: disable=unused-import
 from django.test import TestCase, tag
 from django.urls import reverse
@@ -59,3 +61,51 @@ class TestIndexPage(TestCase):
             f'{reverse("homepage:index", kwargs={"active_category": "physics"})}">Physics</a>'
         )
         self.assertContains(response, find_physics, html=True)
+
+    @tag("login")
+    def test_login_ability(self) -> None:
+        """
+        Test if you can log into in a general manner. It does not test the login page.
+        """
+        user = User.objects.create(username="testuser")
+        user.set_password("12345")
+        user.save()
+
+        self.assertTrue(self.client.login(username="testuser", password="12345"))
+
+    @tag("login", "routing")
+    def test_login_page_200(self) -> None:
+        """
+        Test if the login page is available
+        """
+        response = self.client.get(reverse("auth:login"))
+        self.assertEqual(response.status_code, 200)
+
+    @tag("login", "routing")
+    def test_login_page_redirect(self) -> None:
+        """
+        Test if the login page is available and redirects successfully
+        """
+        user = User.objects.create(username="testuser")
+        user.set_password("12345")
+        user.save()
+
+        response = self.client.post(
+            reverse("auth:login") + "?next=/", {"username": "testuser", "password": "12345"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "homepage/index.html")
+
+    @tag("logout", "routing")
+    def test_logout_page_redirect(self) -> None:
+        """
+        Test if the logout page is available and redirects successfully
+        """
+        user = User.objects.create(username="testuser")
+        user.set_password("12345")
+        user.save()
+
+        self.assertTrue(self.client.login(username="testuser", password="12345"))
+        response = self.client.get(reverse("auth:logout") + "?next=/", follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "homepage/index.html")
--- homepage/views.py
+++ homepage/views.py
@@ -30,5 +30,22 @@ def index(request, active_category: Optional[Category] = None):
         page_title += f" - {active_category.label}"
 
     template_name = "homepage/index.html"
-    context = {"categories": categories, "page_title": page_title}
+    current_url = (
+        reverse("homepage:index")
+        if active_category is None
+        else reverse("homepage:index", kwargs={"active_category": active_category.label.lower()})
+    )
+    if request.user.is_authenticated:
+        loginout = "Logout"
+        loginout_url = reverse("auth:logout") + f"?next={current_url}"
+    else:
+        loginout = "Login"
+        loginout_url = reverse("auth:login") + f"?next={current_url}"
+    context = {
+        "categories": categories,
+        "page_title": page_title,
+        "loginout": loginout,
+        "loginout_url": loginout_url,
+        "current_url": current_url,
+    }
     return render(request, template_name, context)
